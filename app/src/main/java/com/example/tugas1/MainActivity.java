69e230e79b8c15e130f9362f6dabc49c299afdd2
NafarelTM <nafareltriyoga51@gmail.com>
2020-09-30 22:39:36 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugas1/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.tugas1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
