4650c5ebf7e72a4be083fb6f638f7fe954f0474a
NafarelTM <nafareltriyoga51@gmail.com>
2020-09-30 22:43:46 +0700

    - simple changes

--- app/src/main/java/com/example/tugas1/MainActivity.java
+++ app/src/main/java/com/example/tugas1/MainActivity.java
@@ -7,9 +7,12 @@ import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(TAG, "onCreate: dipanggil");
         setContentView(R.layout.activity_main);
     }
 }
